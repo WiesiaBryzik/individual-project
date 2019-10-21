967c0bceaba6736d38e9813ea295ee6e481583fc
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-10-21 10:43:18 +0200

    Add responsiveness

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible);
+}
+
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+  console.log('Link was clicked!');
+});
